ecd500010ff9e18861fbe0b7bc5663b68577bcfd
Kris Kuchinka <kriskuchinka@gmail.com>
2017-03-12 09:06:08 -0700

    Continued fiddling with JS to make it functional and have code be more readable.

--- js/js_countdown.js
+++ js/js_countdown.js
@@ -4,31 +4,27 @@
 // ---> Get countdown date and time working
 
 window.onload = function () {
-	console.log("JavaScript is connected.");
+	console.log("----->JavaScript is connected.<-----");
 
 	// create array of months
 	var mm = [
-		["January"], ["February"], ["March"], ["April"], ["May"], ["June"], ["July"], ["August"], ["September"], ["October"], ["November"], ["December"]
-	];
+		["January"], ["February"], ["March"], ["April"], ["May"], ["June"], ["July"], ["August"], ["September"], ["October"], ["November"], ["December"]];
 	// create array of weekdays
 	var weekDays = [["Sunday"], ["Monday"], ["Tuesday"], ["Wednesday"], ["Thursday"], ["Friday"], ["Saturday"]];
 
-	// create date object
+	// create date object and apply variables to different parts of the date
 	var d = new Date();
-	// get month, day and year values using other built-in date functions
+	var thisWeekDay = (weekDays[d.getDay()]);
 	var thisMonth = (mm[d.getMonth()]);
 	var thisNumericDay =(d.getDate());
 	var thisYear = (d.getFullYear());
-	var weekDay = (d.getDay());
+
 	// get time
 	var currentHour = d.getHours();
 	var currentMinute = d.getMinutes();
-	console.log(currentHour + ":" + currentMinute);
+	
+	var fullDate = thisWeekDay + ", " + thisMonth + " " + thisNumericDay + ", " + thisYear;
 
-	// create combination of the above variables
-	var fullDate = weekDays[weekDay] + ", " + thisMonth + " " + thisNumericDay + ", " + thisYear;
-	console.log(fullDate);
-	// Insert fullDate value into HTML
 	document.getElementById("fullDate").innerHTML = fullDate;
 
 	// make sure that hour is represented as HH:MM, despite if hour is 1 digit
@@ -66,37 +62,38 @@ window.onload = function () {
 	document.getElementById("currentTime").innerHTML = currentTime;
 
 	// enable countdown button to grab date value
-	// var chosenDate;
+	var chosenDate;
 	document.getElementById("chosenDateButton").addEventListener("click", millisecondsToTime);
 	
-	function countDownButton() {
-		console.log("----->Begin countDownButton data.<-----");
-		chosenDate = document.getElementById("chosenDate").value;
-		chosenTime = document.getElementById("chosenTime").value;
-		console.log("chosenDate is: " + chosenDate);
-		console.log("chosenTime is: " + chosenTime);
-		var countDownDate = new Date(chosenDate).getTime();
-		//??????? var countDownDate = chosenDate + " " + chosenTime;
-		console.log("This is the date and time you have chosen: " + countDownDate);
-		var currentTime = new Date().getTime();
-		var timeGap = countDownDate - currentTime;
-		
-		console.log("countDownDate is: " + countDownDate);
-		console.log("currentTime is: " + currentTime);
-		console.log("timeGap is: " + timeGap);
-		var timeDifference = timeGap.getTime();
-		document.getElementById("futureCountdown").innerHTML = timeDifference;
-	}
+// function countDownButton() {
+// 	console.log("----->Begin countDownButton data.<-----");
+	chosenDate = document.getElementById("chosenDate").value;
+	chosenTime = document.getElementById("chosenTime").value;
+// 	console.log("chosenDate is: " + chosenDate);
+// 	console.log("chosenTime is: " + chosenTime);
+// 	var countDownDate = new Date(chosenDate).getTime();
+// 	//??????? var countDownDate = chosenDate + " " + chosenTime;
+// 	console.log("This is the date and time you have chosen: " + countDownDate);
+// 	var currentTime = new Date().getTime();
+// 	var timeGap = countDownDate - currentTime;
+
+// 	console.log("countDownDate is: " + countDownDate);
+// 	console.log("currentTime is: " + currentTime);
+// 	console.log("timeGap is: " + timeGap);
+// 	var timeDifference = timeGap.getTime();
+// 	document.getElementById("futureCountdown").innerHTML = timeDifference;
+// }
 	var countDownDate = new Date(chosenDate).getTime();
-	var currentTime = new Date().getTime();
-	var timeGap = countDownDate - currentTime;
+	console.log("countDownDate: " + countDownDate);
+	var timeNow = new Date().getTime();
+	var timeGap = countDownDate - timeNow;
+	console.log("timeNow: " + timeNow);
 
 	function millisecondsToTime(timeGap) {
 		console.log("----->Begin millisecondsToTime data<-----");
-		var seconds = parseInt((timeGap / 1000) % 60);
-		var minutes = parseInt((timeGap / (1000 * 60)) % 60);
-		var hours = parseInt((timeGap / (1000 * 60 * 60)) % 24);
-		return hours + minutes + seconds;
+		var hours = Math.floor((timeGap % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+		var minutes = Math.floor((timeGap % (1000 * 60 * 60)) / (1000 * 60));
+		var seconds = Math.floor((timeGap % (1000 * 60)) / 1000);
 		console.log("Hours: " + hours + " Minutes: " + minutes + " Seconds: " + seconds);
 	}
 
